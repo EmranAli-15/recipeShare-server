2dbb6b62e5b2db214e91aec5e6ba044aec9b5bad
EmranAli-15 <mohammademranali15@gmail.com>
2024-10-11 17:05:29 +0600

    working on recipe upload

---
+++ src/app/modules/recipe/recipe.interface.ts
@@ -0,0 +1,10 @@
+export type TRecipe = {
+    title: string;
+    image: string;
+    like: number;
+    comment: number;
+    rating: number;
+    recipe: string;
+    comments: string[];
+    isDeleted: boolean,
+};
